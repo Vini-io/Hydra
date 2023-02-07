84894255aa0f0df2b9ab2117bfa4f44a35cefe96
Vini-io <barbosavini2@gmail.com>
2023-02-07 16:29:41 -0300

    finish intro

--- src/components/Introduction/script.js
+++ src/components/Introduction/script.js
@@ -1,10 +1,26 @@
 
 import './style.css'
+import imageIntro from './assets/image/virt.svg'
 
-function Introduction(){
-    return(
-        <div>
-            <h1>Olá Mundo</h1>
+function Introduction() {
+    return (
+        <div className='container'>
+
+            <div className='about'>
+                <h1><span>Dive</span> Into The Depths Of <span>Virtual Reality</span></h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore
+                    nisl tincidunt eget. Lectus mauris eros in vitae .
+                </p>
+                <button>BUILD YOUR WORLD</button>
+            </div>
+
+            <div className='imageIntro'>
+                <img src={imageIntro} />
+            </div>
+
+
         </div>
     )
 }
